87ba9edafb24f50a76d37c226836abe912a1f1e0
Aryan Sethi <aryan.rdps@gmail.com>
2022-05-15 23:53:59 +0530

    Added mongoose mongodb and setup connection

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 const PORT_NUMBER = 8800;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+const connect = async () => {   
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to mongoDB.");
+    } catch (error) {
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected" , () => {
+    console.log("MongoDB disconnected");
+})
+mongoose.connection.on("connected" , () => {
+    console.log("MongoDB connected");
+})
 
 app.listen(PORT_NUMBER, () => {
-    console.log("Connected to backend");
+    connect();
+    console.log("Connected to backend!");
 });
