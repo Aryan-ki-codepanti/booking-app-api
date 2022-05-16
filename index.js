fbdb26408fc843432ddfc1d48d970b705a3b44ec
Aryan Sethi <aryan.rdps@gmail.com>
2022-05-16 09:45:20 +0530

    Added hotel schema

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from './routes/auth.js';
+import usersRoutes from './routes/users.js';
+import hotelsRoutes from './routes/hotels.js';
+import roomsRoutes from './routes/rooms.js';
 
 const app = express();
 dotenv.config();
 const PORT_NUMBER = 8800;
 const MONGO_URI = process.env.MONGO_URI;
 
-
-const connect = async () => {   
+const connect = async () => {
     try {
         await mongoose.connect(MONGO_URI);
         console.log("Connected to mongoDB.");
     } catch (error) {
         throw error;
     }
-}
+};
 
-mongoose.connection.on("disconnected" , () => {
+mongoose.connection.on("disconnected", () => {
     console.log("MongoDB disconnected");
-})
-mongoose.connection.on("connected" , () => {
-    console.log("MongoDB connected");
-})
+});
+
+
+// middlewares
+app.use("/api/auth" , authRoutes);
+app.use("/api/users" , usersRoutes);
+app.use("/api/hotels" , hotelsRoutes);
+app.use("/api/rooms" , roomsRoutes);
 
 app.listen(PORT_NUMBER, () => {
     connect();
