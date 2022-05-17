5be1fab3ea75312a26e0ca2095c7205e809621ed
Aryan Sethi <aryan.rdps@gmail.com>
2022-05-17 17:29:05 +0530

    Added middlewares to verify admins and users and tokens

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,25 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import {
+    deleteUser,
+    getUser,
+    getUsers,
+    updateUser
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauth", verifyToken, (req, res, next) => {
+    res.send("You are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("Hello  user , you can delete this account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("Hello admin , you are logged in can delete all accounts");
+});
 
 // UPDATE
 router.put("/:id", updateUser);
